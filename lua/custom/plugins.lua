c587565cc34a4efa9887f50a3515b38589f2f3e1
Lucas Vázquez López <lucasvlopez@gmail.com>
2023-06-09 12:02:25 +0200

    Add files via upload

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,7 +1,17 @@
 local plugins = {
   {
     "vim-crystal/vim-crystal",
-    ft = "crystal"
+    ft = "crystal",
+    config = function (_)
+      vim.g.crystal_auto_format = 1
+    end
+  },
+  {
+    "varnishcache-friends/vim-varnish",
+    ft = "vcl",
+    config = function (_)
+      vim.g.vcl_auto_format = 1
+    end
   }
 }
 return plugins
